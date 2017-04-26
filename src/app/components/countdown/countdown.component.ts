c45bd374045d6da21c93eabf72769691c6fbd4be
Dharmik Patel <dvp236@nyu.edu>
2017-04-25 23:43:19 -0400

    Fix- countdown- resolved safari binding issue

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ChangeDetectorRef } from '@angular/core';
+
 
 declare var moment: any;
 
@@ -8,18 +9,18 @@ declare var moment: any;
   styleUrls: ['./countdown.component.css']
 })
 export class CountdownComponent implements OnInit{
-  oneDay = 24*60*60*1000;
+  // oneDay = 24*60*60*1000;
+  //
+  // today = new Date();
+  // mday = new Date(2018,1,22);
 
-  today = new Date();
-  mday = new Date(2018,1,22);
+  remDays = 0;
+  now = null;
+  end = null;
+  duration = null;
+  daysm = 0;
 
-  days=0;
 
-  now = moment(new Date()); //todays date
-  end = moment("2018-1-22"); // another date
-  duration = moment.duration(this.end.diff(this.now));
-  daysm = this.duration.asDays();
-  remDays = Math.ceil(this.daysm);
 
 
   constructor() {
@@ -32,7 +33,18 @@ export class CountdownComponent implements OnInit{
   // console.log("rem days: "+this.days);
   // console.log("dates "+this.today+"---"+this.mday);
   // console.log(this.daysm);
+      setTimeout(()=> this.do(), 500);
   }
 
+
+  do(){
+    this.now = moment(new Date()); //todays date
+    //console.log (new Date('2011-04-12'.replace(/-/g, "/")));
+    this.end = moment(new Date('2018-01-22'.replace(/-/g, "/"))); // another date
+    this.duration = moment.duration(this.end.diff(this.now));
+    this.daysm = this.duration.asDays();
+    this.remDays = Math.ceil(this.daysm);
+
+  }
 
 }
